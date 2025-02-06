285a7e2d21faea6f9988c3ca23e3e1cd38f0ab3d
MorenoFalo14 <morenofalo5@gmail.com>
2025-02-06 01:09:31 +0100

    Sistema de gestão curricular

---
+++ IMTSB/script IMTSB.js
@@ -0,0 +1,6 @@
+// JavaScript code for toggling the menu
+function toggleMenu() {
+    console.log("Menu toggle function called"); // Debugging line
+    const menu = document.querySelector('.navbar .menu');
+    menu.classList.toggle('show');
+}
