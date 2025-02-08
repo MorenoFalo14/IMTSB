c3ae1e523710e9d8be04641f5d64871511bbaa16
MorenoFalo14 <morenofalo5@gmail.com>
2025-02-08 05:04:21 +0100

    Update script PROFS.js

--- IMTSB/script PROFS.js
+++ IMTSB/script PROFS.js
@@ -4,30 +4,3 @@ function toggleMenu() {
     const menu = document.querySelector('.navbar .menu');
     menu.classList.toggle('show');
 }
-const sliderContainer = document.querySelector('.slider-container');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-let currentIndex = 0;
-
-function showSlide(index) {
-    const totalSlides = sliderContainer.children.length;
-    if (index >= totalSlides) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else {
-        currentIndex = index;
-    }
-    const offset = -currentIndex * 270; // 250px width + 20px margin
-    sliderContainer.style.transform = `translateX(${offset}px)`;
-}
-
-prevButton.addEventListener('click', () => {
-    showSlide(currentIndex - 1);
-});
-
-nextButton.addEventListener('click', () => {
-    showSlide(currentIndex + 1);
-});
-
-showSlide(currentIndex);
